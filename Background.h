c05c037e4e2056e7ab75811928684f65d03df54b
joyce910718 <79899933+joyce910718@users.noreply.github.com>
2021-04-28 21:47:58 +0800

    Add files via upload

---
+++ Background.h
@@ -0,0 +1,10 @@
+#ifndef Background_h
+#define Background_h
+#include<iostream>
+using namespace std;
+class Background {
+public:
+	void print();
+
+};
+#endif
---
+++ ooxx.cpp
@@ -0,0 +1,8 @@
+#include "Background.h"
+#include<iostream>
+using namespace std;
+
+int main() {
+	Background x;
+	x.print();
+}
