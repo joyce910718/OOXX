b396d5644e629e743c014889c21e843296205800
amazingOMG <79899903+amazingOMG@users.noreply.github.com>
2021-05-08 02:11:31 +0800

    Update TictactoeBegin.cpp
    - 更改執行後的輸出排版
    
    - debug :
                   - 之前出現「玩家選位提醒」時 選位提醒的玩家姓名 與 實際在選位的玩家姓名不符的現象   #已解決 👌
    
    ********************************************************************************************************************************
    
    #待解決 🥱😱
    - 於 PlayerInitialization 類別：
                                                      -玩家選擇遊戲選擇模式時，不可輸入除了 1 、2 以外的數字
                                                        否則請玩家重新輸入
    
                                                      -玩家1 與 玩家2 若名子重複
                                                        需輸出：此名稱已被使用，請重新輸入
                                                        並讓玩家重新輸入
    
                                                      -玩家選擇棋子時，不可輸入 O 或 X 以外的字元，若出現不符合規定的字元
                                                        需輸出：此符號不符合棋子的規格
                                                        並請玩家重新輸入

--- TictactoeBegin.cpp
+++ TictactoeBegin.cpp
@@ -1,6 +1,7 @@
 #include "PlayerInitialization.h"
 #include "TictactoeBegin.h"
 #include<iostream>
+#include<iomanip>
 #include<string>
 using namespace std;
 
@@ -13,15 +14,15 @@ TictactoeBegin::TictactoeBegin() {
 }
 
 void TictactoeBegin::print() {
-	cout << "\n     |     |     " << endl;
-	cout << "  " << array[1] << "  |  " << array[2] << "  |  " << array[3] << endl;
-	cout << "_____|_____|_____" << endl;
-	cout << "     |     |     " << endl;
-	cout << "  " << array[4] << "  |  " << array[5] << "  |  " << array[6] << endl;
-	cout << "_____|_____|_____" << endl;
-	cout << "     |     |   " << endl;
-	cout << "  " << array[7] << "  |  " << array[8] << "  |  " << array[9] << endl;
-	cout << "     |     |   \n\n" << endl;
+	cout << "\n\n" <<  setw(34) << " " << "     |     |     " << endl;
+	cout << setw(34) << " " << "  " << array[1] << "  |  " << array[2] << "  |  " << array[3] << endl;
+	cout << setw(34) << " " << "_____|_____|_____" << endl;
+	cout << setw(34) << " " << "     |     |     " << endl;
+	cout << setw(34) << " " << "  " << array[4] << "  |  " << array[5] << "  |  " << array[6] << endl;
+	cout << setw(34) << " " << "_____|_____|_____" << endl;
+	cout << setw(34) << " " << "     |     |   " << endl;
+	cout << setw(34) << " " << "  " << array[7] << "  |  " << array[8] << "  |  " << array[9] << endl;
+	cout << setw(34) << " " << "     |     |   \n\n" << endl;
 
 }
 
@@ -29,48 +30,46 @@ void TictactoeBegin::choiceLayingDownPieces() {
 
 	gameInitialization.print();
 
-	( gameInitialization.getPlayer1_Chessman() == "X" ) ? offensive = gameInitialization.getPlayer1_Name() : offensive = gameInitialization.getPlayer2_Name();
-	( gameInitialization.getPlayer1_Chessman() == "O" ) ? defensive = gameInitialization.getPlayer1_Name() : defensive = gameInitialization.getPlayer2_Name();
+	(gameInitialization.getPlayer1_Chessman() == "X") ? offensive = gameInitialization.getPlayer1_Name() : offensive = gameInitialization.getPlayer2_Name();
+	(gameInitialization.getPlayer1_Chessman() == "O") ? defensive = gameInitialization.getPlayer1_Name() : defensive = gameInitialization.getPlayer2_Name();
 
-		while (loop < 9) {
+	while (loop < 9) {
 
-			switch (loop % 2) {
-				case 0: cout << offensive << " 請輸入落子位置："; break;
-				case 1: cout << defensive << " 請輸入落子位置："; break;
+		switch (loop % 2) {
+			case 0: cout <<  setw(20) << "\t" << offensive << " 請輸入落子位置："; break;
+			case 1: cout << setw(20) << "\t" << defensive << " 請輸入落子位置："; break;
+		}
+		cin >> space;
+
+		if (array[space] == 'O' || array[space] == 'X')
+			cout << "\n" << setw(30) << " " << "                  此格已有棋子，請重選一格\n\n" << endl;
+		else {
+			if (space > 9 || space < 1) 
+				cout << "\n" << setw(30) << " " << "                  你的棋子擺放位置已超出範圍，請重新選位\n\n" << endl;
+			else {
+					switch (loop % 2) {
+						case 0: array[space] = 'X'; break;
+						case 1: array[space] = 'O'; break;
+					}
+				print();
+				loop++;
 			}
-			cin >> space;
 
-			if ( array[space] == 'O' || array[space] == 'X' ) {
-				cout << "\n                輸入錯誤\n\n" << endl;
-			}else {
-						if ( space > 9 || space < 1 ) {
-							cout << "\n你的棋子擺放位置已超出範圍，請重新選位\n\n" << endl;
-						}else {
-							switch ( loop % 2 ) {
-								case 0: array[space] = 'X'; break;
-								case 1: array[space] = 'O'; break;
-							}
-						}
-		
-					print();
+			if ((array[1] == array[2] && array[1] == array[3] && array[1] != ' ') ||
+				(array[4] == array[5] && array[4] == array[6] && array[4] != ' ') ||
+				(array[7] == array[8] && array[7] == array[9] && array[7] != ' ') ||
+				(array[1] == array[4] && array[1] == array[7] && array[1] != ' ') ||
+				(array[2] == array[5] && array[2] == array[8] && array[2] != ' ') ||
+				(array[3] == array[6] && array[3] == array[9] && array[3] != ' ') ||
+				(array[1] == array[5] && array[1] == array[9] && array[1] != ' ') ||
+				(array[3] == array[5] && array[3] == array[7] && array[3] != ' ')) {
 
-						if ((array[1] == array[2] && array[1] == array[3] && array[1] != ' ') ||
-							(array[4] == array[5] && array[4] == array[6] && array[4] != ' ') ||
-							(array[7] == array[8] && array[7] == array[9] && array[7] != ' ') ||
-							(array[1] == array[4] && array[1] == array[7] && array[1] != ' ') ||
-							(array[2] == array[5] && array[2] == array[8] && array[2] != ' ') ||
-							(array[3] == array[6] && array[3] == array[9] && array[3] != ' ') ||
-							(array[1] == array[5] && array[1] == array[9] && array[1] != ' ') ||
-							(array[3] == array[5] && array[3] == array[7] && array[3] != ' ')) {
-							
-							peace++;
-							
-							( loop % 2 == 0 ) ? cout << offensive <<" 獲勝\n" : cout << defensive << " 獲勝\n";
-							break;
-						}
+				peace++;
 
-					loop++;
+				(loop % 2 == 0) ? cout << setw(20) << "\t" << "進攻方: " << offensive << " 獲勝\n" : cout << setw(20) << "\t" << "防守方: " << defensive << " 獲勝\n";
+				break;
 			}
 		}
-		(peace == 0) ? cout << "平手啦呵呵呵" : cout << endl;
+	}
+	(peace == 0) ? cout << setw(20) << "\t" << "平手啦呵呵呵" << endl : cout << endl;
 }
