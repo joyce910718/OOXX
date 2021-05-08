f0ca23909eafb51ffda9776769743ef1329a1bcd
amazingOMG <79899903+amazingOMG@users.noreply.github.com>
2021-05-08 14:23:37 +0800

    Update TictactoeBegin.h
    增加註解

--- TictactoeBegin.h
+++ TictactoeBegin.h
@@ -9,16 +9,16 @@ using namespace std;
 class TictactoeBegin {
 
 private:
-	char *array;
-	static int peace;
-	int space = 0, loop = 0;
-	string offensive, defensive;
-	PlayerInitialization gameInitialization;
+	char *array;									//將九宮格的值存入此變數中
+	static bool peace;								//此變數功能為：判斷遊戲是否為平局，值為0表示平局、值為1表示已分出勝負
+	int space = 0, loop = 0;						//space 功能為：紀錄使用者輸入的下棋位子	  loop 功能為：判斷目前由哪位玩家落子
+	string offensive, defensive;					//offensive 功能為：紀錄進攻方的玩家名稱      offensive 功能為：紀錄防守方的玩家名稱
+	PlayerInitialization gameInitialization;		//gameInitialization 功能為：呼叫 PlayerInitialization類別，用以讀取當中的玩家資料
 
 public:
-	TictactoeBegin();
-	void print();
-	void choiceLayingDownPieces();
-	bool Gameover();
+	TictactoeBegin();								//建構子
+	void print();									//此函式功能為：匯出九宮格
+	void choiceLayingDownPieces();					//此函式功能為：玩家選擇落子位置
+	bool Gameover();								//此函式功能為：判斷勝負
 };
 #endif
