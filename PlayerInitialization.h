7c8dffe9cb75dd4537cf9598f31bc8883d635d3f
amazingOMG <79899903+amazingOMG@users.noreply.github.com>
2021-05-08 15:07:58 +0800

    Update PlayerInitialization.h
    加註解

--- PlayerInitialization.h
+++ PlayerInitialization.h
@@ -6,14 +6,14 @@ using namespace std;
 
 class PlayerInitialization {
 private:
-	int pickAMode;
-	string Player1_Name, Player2_Name, Player1_Chessman, Player2_Chessman;
+	int pickAMode;															//此變數功能為：讓玩家選擇遊戲模式
+	string Player1_Name, Player2_Name, Player1_Chessman, Player2_Chessman;	//Name：玩家名稱		Chessman：玩家所選擇的棋子
 
 public:
 	PlayerInitialization();
-	void print_loading();
-	void print();
-	string getPlayer1_Chessman() const;
+	void print_loading();					//此函式功能為：輸出「遊戲加載中」的樣式
+	void print();							//此函式功能為：設定使用者的基本資料，並匯出
+	string getPlayer1_Chessman() const;		
 	string getPlayer2_Chessman() const;
 	string getPlayer1_Name() const;
 	string getPlayer2_Name() const;
