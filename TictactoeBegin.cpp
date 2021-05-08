e377fcfe2c03c288f03f1b2ddb9f2a9ec4a87784
amazingOMG <79899903+amazingOMG@users.noreply.github.com>
2021-05-08 14:25:21 +0800

    Update TictactoeBegin.cpp
    加註解

--- TictactoeBegin.cpp
+++ TictactoeBegin.cpp
@@ -5,9 +5,9 @@
 #include<string>
 using namespace std;
 
-int TictactoeBegin::peace = 0;
+bool TictactoeBegin::peace = false;
 
-TictactoeBegin::TictactoeBegin() {
+TictactoeBegin::TictactoeBegin() {					//將 array 變數的值初始化為空格字元
 	array = new char[10];
 
 	for (int i = 1; i <= 9; i++) {
@@ -15,15 +15,15 @@ TictactoeBegin::TictactoeBegin() {
 	}
 }
 
-void TictactoeBegin::print() {
+void TictactoeBegin::print() {						//匯出九宮格
 	cout << "\n\n" <<  setw(34) << " " << "     |     |     " << endl;
-	cout << setw(34) << " " << "  " << array[1] << "  |  " << array[2] << "  |  " << array[3] << endl;
+	cout << setw(34) << " " << "  " << array[7] << "  |  " << array[8] << "  |  " << array[9] << endl;
 	cout << setw(34) << " " << "_____|_____|_____" << endl;
 	cout << setw(34) << " " << "     |     |     " << endl;
 	cout << setw(34) << " " << "  " << array[4] << "  |  " << array[5] << "  |  " << array[6] << endl;
 	cout << setw(34) << " " << "_____|_____|_____" << endl;
 	cout << setw(34) << " " << "     |     |   " << endl;
-	cout << setw(34) << " " << "  " << array[7] << "  |  " << array[8] << "  |  " << array[9] << endl;
+	cout << setw(34) << " " << "  " << array[1] << "  |  " << array[2] << "  |  " << array[3] << endl;
 	cout << setw(34) << " " << "     |     |   \n\n" << endl;
 
 }
@@ -32,33 +32,33 @@ void TictactoeBegin::choiceLayingDownPieces() {
 
 	gameInitialization.print();
 
-	(gameInitialization.getPlayer1_Chessman() == "X") ? offensive = gameInitialization.getPlayer1_Name() : offensive = gameInitialization.getPlayer2_Name();
-	(gameInitialization.getPlayer1_Chessman() == "O") ? defensive = gameInitialization.getPlayer1_Name() : defensive = gameInitialization.getPlayer2_Name();
+	(gameInitialization.getPlayer1_Chessman() == "X") ? offensive = gameInitialization.getPlayer1_Name() : offensive = gameInitialization.getPlayer2_Name();	//進攻者的玩家名稱設給offensive變數
+	(gameInitialization.getPlayer1_Chessman() == "O") ? defensive = gameInitialization.getPlayer1_Name() : defensive = gameInitialization.getPlayer2_Name();	//防守方的玩家名稱設給defensive變數
 
 	while (loop < 9) {
 
-		switch (loop % 2) {
-			case 0: cout <<  setw(20) << "\t" << offensive << " 請輸入落子位置："; break;
-			case 1: cout << setw(20) << "\t" << defensive << " 請輸入落子位置："; break;
+		switch (loop % 2) {		//判斷判斷目前由哪位玩家落子
+			case 0: cout <<  setw(20) << "\t" << "(進攻方) " << offensive << " 請輸入落子位置："; break;
+			case 1: cout << setw(20) << "\t" << "(防守方) " << defensive << " 請輸入落子位置："; break;
 		}
 		cin >> space;
 
-		if (array[space] == 'O' || array[space] == 'X')
+		if (array[space] == 'O' || array[space] == 'X')		//玩家選擇的位置已有棋子
 			cout << "\n" << setw(30) << " " << "                  此格已有棋子，請重選一格\n\n" << endl;
-		else {
-			if (space > 9 || space < 1) 
+		else {		//玩家選擇的位置是空的
+			if (space > 9 || space < 1)		//玩家選擇的位置是空的，但已超出可選擇的範圍，需重新輸入
 				cout << "\n" << setw(30) << " " << "                  你的棋子擺放位置已超出範圍，請重新選位\n\n" << endl;
-			else {
-					switch (loop % 2) {
+			else {	//遊戲正常進行
+					switch (loop % 2) { //判斷判斷目前由哪個棋子落子
 						case 0: array[space] = 'X'; break;
 						case 1: array[space] = 'O'; break;
 					}
 
-				print();
-				loop++;
+				print();	// 匯出九宮格
+				loop++;		// loop +1 換另一位玩家玩家落子
 			}
 
-			if (Gameover()) {
+			if (Gameover()) {	//如果已有玩家勝利，則進行 if 內的程式碼，並跳出迴圈
 				(loop % 2 == 0) ? cout << setw(20) << "\t" << "進攻方： " << offensive << " 獲勝\n" : cout << setw(20) << "\t" << "防守方： " << defensive << " 獲勝\n";
 				break;
 			}	
@@ -67,7 +67,7 @@ void TictactoeBegin::choiceLayingDownPieces() {
 	(peace == 0) ? cout << setw(20) << "\t" << "平手啦呵呵呵" << endl : cout << endl;
 }
 
-bool TictactoeBegin::Gameover() {
+bool TictactoeBegin::Gameover() {	//此函式功能為：判斷勝負 
 	if ((array[1] == array[2] && array[1] == array[3] && array[1] != ' ') ||
 		(array[4] == array[5] && array[4] == array[6] && array[4] != ' ') ||
 		(array[7] == array[8] && array[7] == array[9] && array[7] != ' ') ||
@@ -77,7 +77,7 @@ bool TictactoeBegin::Gameover() {
 		(array[1] == array[5] && array[1] == array[9] && array[1] != ' ') ||
 		(array[3] == array[5] && array[3] == array[7] && array[3] != ' ')) {
 
-		peace++;
+		peace = true;	
 		return true;
 	}
 	else
