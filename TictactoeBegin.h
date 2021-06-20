8c404138457a4488c155f81e31c78b3448069e47
amazingOMG <79899903+amazingOMG@users.noreply.github.com>
2021-06-20 11:34:31 +0800

    Update TictactoeBegin.h
    因主程式需要，特地增加幾個函式

--- TictactoeBegin.h
+++ TictactoeBegin.h
@@ -8,20 +8,23 @@ using namespace std;
 class TictactoeBegin : public PlayerInitialization{
 
 private:
-	char *array;									//將九宮格的值存入此變數中
-	static bool peace;								//此變數功能為：判斷遊戲是否為平局，值為0表示平局、值為1表示已分出勝負
-	int space = 0, loop = 0;						//space 功能為：紀錄使用者輸入的下棋位子	  loop 功能為：判斷目前由哪位玩家落子
+	char *arr;										//將九宮格的值存入此陣列中
+	bool peace = true;								//此變數功能為：判斷遊戲是否為平局，值為0表示平局、值為1表示已分出勝負
+	int space = 0;									//space 功能為：紀錄使用者輸入的下棋位子
 	string offensive, defensive;					//offensive 功能為：紀錄進攻方的玩家名稱      offensive 功能為：紀錄防守方的玩家名稱
 
 public:
 	TictactoeBegin();								//建構子
 	void setOffensive();							//此函式功能為：將進攻者的姓名設置給變數 offensive
-	void setDefensive();							//此函式功能為：將防守者的姓名設置給變數 defensive
 	string getOffensive() const;					//此函式功能為：回傳進攻方的姓名
+	void setDefensive();							//此函式功能為：將防守者的姓名設置給變數 defensive
 	string getDefensive() const;					//此函式功能為：回傳防守方的姓名
+	void setSpace( int );							//此函式功能為：設置玩家選擇的落子位置
+	int getSpace() const;							//此函式功能為：回傳玩家所選擇的九宮格位置
 	bool isSpaceFull();								//此函式功能為：判斷此格是否已有其他棋子
 	void print();									//此函式功能為：匯出九宮格
-	void choiceLayingDownPieces();					//此函式功能為：玩家選擇落子位置
-	bool Gameover();								//此函式功能為：判斷勝負
+	void PutDown(char);								//此函式功能為：將玩家輸入的落子位子與對應到的棋子，紀錄至陣列中
+	bool isGameover();								//此函式功能為：判斷勝負
+	bool isPeace();									//此函式功能為：回傳是否為平局
 };
 #endif
