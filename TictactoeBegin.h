1700d8e3cd7714fbe401290472f5b51c4ac2b0ca
amazingOMG <79899903+amazingOMG@users.noreply.github.com>
2021-06-20 09:35:28 +0800

    Update TictactoeBegin.h
    增加四個函式：
    void setOffensive();
    void setDefensive();	
    string getOffensive() const;
    string getDefensive() const;

--- TictactoeBegin.h
+++ TictactoeBegin.h
@@ -2,21 +2,23 @@
 #define TictactoeBegin_h
 
 #include "PlayerInitialization.h"
-#include<iostream>
 #include<string>
 using namespace std;
 
-class TictactoeBegin {
+class TictactoeBegin : public PlayerInitialization{
 
 private:
 	char *array;									//將九宮格的值存入此變數中
 	static bool peace;								//此變數功能為：判斷遊戲是否為平局，值為0表示平局、值為1表示已分出勝負
 	int space = 0, loop = 0;						//space 功能為：紀錄使用者輸入的下棋位子	  loop 功能為：判斷目前由哪位玩家落子
 	string offensive, defensive;					//offensive 功能為：紀錄進攻方的玩家名稱      offensive 功能為：紀錄防守方的玩家名稱
-	PlayerInitialization gameInitialization;		//gameInitialization 功能為：呼叫 PlayerInitialization類別，用以讀取當中的玩家資料
 
 public:
 	TictactoeBegin();								//建構子
+	void setOffensive();							//此函式功能為：將進攻者的姓名設置給變數 offensive
+	void setDefensive();							//此函式功能為：將防守者的姓名設置給變數 defensive
+	string getOffensive() const;					//此函式功能為：回傳進攻方的姓名
+	string getDefensive() const;					//此函式功能為：回傳防守方的姓名
 	void print();									//此函式功能為：匯出九宮格
 	void choiceLayingDownPieces();					//此函式功能為：玩家選擇落子位置
 	bool Gameover();								//此函式功能為：判斷勝負
